324989fb60fc1e090a7c81609f4a06e5fcb42ebd
Preston Edmands <pedmands@gmail.com>
2016-05-20 19:16:08 -0400

    Styled header and added custom logo

--- inc/custom-header.php
+++ inc/custom-header.php
@@ -21,11 +21,17 @@
  * @uses whitenoise_header_style()
  */
 function whitenoise_custom_header_setup() {
+	add_theme_support('custom-logo', array (
+		'width' => 96,
+		'height' => 96,
+		'flex-width' => false,
+		'flex-height' => false, 
+	) );
 	add_theme_support( 'custom-header', apply_filters( 'whitenoise_custom_header_args', array(
 		'default-image'          => '',
-		'default-text-color'     => '000000',
-		'width'                  => 1000,
-		'height'                 => 250,
+		'default-text-color'     => 'ffffff',
+		'width'                  => 1600,
+		'height'                 => 420,
 		'flex-height'            => true,
 		'wp-head-callback'       => 'whitenoise_header_style',
 	) ) );
